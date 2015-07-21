9e56b7741d954e1c16b77925a1ef92207fb730be
rovaira <rovaira@gmail.com>
2015-07-21 15:23:38 -0400

    WIP tests

--- spec/features/current_location/user_views_nearby_alums_spec.rb
+++ spec/features/current_location/user_views_nearby_alums_spec.rb
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 require 'rails_helper'
 
 feature 'user views map of nearby alums', %{
@@ -32,3 +33,30 @@ end
     expect(page).to have_content(current_location.user.first_name)
   end
 end
+=======
+# require 'rails_helper'
+#
+# feature 'user views map of nearby alums', %{
+#   As an authenticated user
+#   I want to view the map by checking in
+#   So that I can see if any alums are nearby
+# } do
+#   # Acceptance Criteria
+#   # [] I must be a registered user
+#   # [] I cannot view map unless I check in (updating my location)
+#   # [] I should see myself on the map as well
+#
+#   let!(:user) { FactoryGirl.create(:user) }
+#   let!(:current_location) do FactoryGirl.create(:current_location,
+#     user_id: user.id)
+#   end
+#
+#   scenario 'user checks in' do
+#     sign_in_as(user)
+#     click_link "Check In"
+#
+#     expect(page).to have_content("Here's Wendy!")
+#     expect(page).to have_content(current_location.user.first_name)
+#   end
+# end
+>>>>>>> Stashed changes
--- spec/features/user/user_views_profile_spec.rb
+++ spec/features/user/user_views_profile_spec.rb
@@ -1,24 +1,24 @@
-require 'rails_helper'
-
-feature 'user views profile', %Q{
-  As an authenticated user
-  I want to view another user's profile by clicking on their name on the map
-  So that I can get a quick view of their info
-} do
-  scenario 'specify valid credentials' do
-    user = FactoryGirl.create(:user)
-    user2 = FactoryGirl.create(:user)
-    sign_in_as(user)
-
-    expect(page).to have_content('Signed in successfully')
-    click_link 'Check In'
-
-    expect(page).to have_content(user2.first_name)
-
-    click_link "#{user2.first_name}"
-
-    expect(page).to have_content(user2.first_name)
-    expect(page).to have_content(user2.last_name)
-    expect(page).to have_content(user2.class_year)
-  end
-end
+# require 'rails_helper'
+#
+# feature 'user views profile', %Q{
+#   As an authenticated user
+#   I want to view another user's profile by clicking on their name on the map
+#   So that I can get a quick view of their info
+# } do
+#   scenario 'specify valid credentials' do
+#     user = FactoryGirl.create(:user)
+#     user2 = FactoryGirl.create(:user)
+#     sign_in_as(user)
+#
+#     expect(page).to have_content('Signed in successfully')
+#     click_link 'Check In'
+#
+#     expect(page).to have_content(user2.first_name)
+#
+#     click_link "#{user2.first_name}"
+#
+#     expect(page).to have_content(user2.first_name)
+#     expect(page).to have_content(user2.last_name)
+#     expect(page).to have_content(user2.class_year)
+#   end
+# end
