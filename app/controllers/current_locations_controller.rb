9cbb2b0a99d8b44923fd9d98e780ceba054d9c0d
rovaira <rovaira@gmail.com>
2015-07-21 00:31:24 -0400

    add html5 geolocation script

--- app/controllers/current_locations_controller.rb
+++ app/controllers/current_locations_controller.rb
@@ -49,6 +49,13 @@ class CurrentLocationsController < ApplicationController
     redirect_to parks_url, notice: "Successfully destroyed user's location."
   end
 
+  def geocode
+    position = Geocoder.coordinates(params[:query])
+    respond_to do |wants|
+      wants.json { render :json => position }
+    end
+  end
+
   private
 
   def current_location_params
--- app/models/current_location.rb
+++ app/models/current_location.rb
@@ -1,6 +1,7 @@
 class CurrentLocation < ActiveRecord::Base
   belongs_to :user
   geocoded_by :address
+  reverse_geocoded_by :latitude, :longitude
 
   after_validation :geocode, if: :address_changed?
 end
