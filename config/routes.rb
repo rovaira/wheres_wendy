b1ec90fa4dae6c6674c3a776685a8d9d3e85e747
rovaira <rovaira@gmail.com>
2015-07-24 13:33:57 -0400

    fix users_controller#update strong params to include all the fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
-  resources :users, only: [:show, :update, :edit, :destroy]
+  resources :users, only: [:destroy, :show, :edit, :update]
   resources :current_locations, only: [:index, :update]
   resources :pings
 
