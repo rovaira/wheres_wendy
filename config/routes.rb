dd1e19505c7d9c4d579c25babec463007a75db81
rovaira <rovaira@gmail.com>
2015-07-24 16:38:37 -0400

    post hound 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#index'
 
-  devise_for :users, controllers: {sessions: 'my_devise/sessions'}
+  devise_for :users, controllers: { sessions: 'my_devise/sessions' }
   resources :users, only: [:show, :update, :edit, :destroy]
   resources :current_locations, only: [:index, :update]
   resources :pings
