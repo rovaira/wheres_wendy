49dc91688220ad966ac2ba630034f44098cb34a6
rovaira <rovaira@gmail.com>
2015-07-24 13:50:15 -0400

    Fix CurrentLocations controller#update so that a current_location object is created if needed when updating user's current_location attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,5 @@
 class UsersController < ApplicationController
 
-  # def new
-  #   @user = User.new
-  # end
-  #
-  # def create
-  #   @user = User.new(user_params)
-  #   CurrentLocation.new(user_id: current_user.id)
-  #   if @user.save
-  #     redirect_to @user,``
-  #       flash[:notice] = "Successfully created user location."
-  #   else
-  #     render :action => 'new'
-  #   end
-  # end
-
   def destroy
     if current_user.try(:admin?)
       @user = User.find(params[:id])
