ff605dcd7039cabf094bd41be65ca4c926d31073
rovaira <rovaira@gmail.com>
2015-07-28 21:28:09 -0400

    Texting works! Still using free trial account. Refactored body of text.

--- app/controllers/pings_controller.rb
+++ app/controllers/pings_controller.rb
@@ -8,9 +8,6 @@ class PingsController < ApplicationController
   def create
     receiver = User.find(params["ping"]["receiver_id"])
     @ping = Ping.new(ping_params)
-    twilio_sid = ENV["TWILIO_ACCOUNT_SID"]
-    twilio_token = ENV["TWILIO_AUTH_TOKEN"]
-    twilio_phone_number = ENV["TWILIO_PHONE_NUMBER"]
 
     @client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
 
@@ -19,7 +16,13 @@ class PingsController < ApplicationController
       @client.messages.create(
         from: ENV["TWILIO_PHONE_NUMBER"],
         to: "+1#{receiver.phone}",
-        body: "Hello #{receiver.first_name}! SENDER from the Class of YEAR is nearby and says hello. Message back using their information below to say hi and keep the lamp burning! PHONE NUMBER/EMAIL"
+        body: %{
+          Hello #{receiver.first_name}! #{@ping.sender.first_name} from the
+          Class of #{@ping.sender.class_year} is nearby and says hello. Message
+          back using their info below to say hi and keep the lamp burning!
+          #{ping.sender.phone}
+          #{ping.sender.email}
+        }
       )
       flash[:notice] = "Successfully pinged #{receiver.first_name}."
       redirect_to user_path(receiver.id)
@@ -34,4 +37,17 @@ class PingsController < ApplicationController
   def ping_params
     params.require(:ping).permit(:sender_id, :receiver_id)
   end
+
+  # def text_body(ping)
+  #   %{
+  #     "Hello #{receiver.first_name}! #{ping.sender.first_name} from the Class
+  #     of #{ping.sender.class_year} is nearby and says hello.
+  #
+  #     Message back using their information below to say hi and keep the
+  #     lamp burning!
+  #
+  #     #{ping.sender.phone}
+  #     #{ping.sender.email}
+  #   }
+  # end
 end
